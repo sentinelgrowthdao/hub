8d9d911383397def8d67b31198b5491aa5867b67
Tony Stark <ironman0x7b2@protonmail.com>
2024-05-24 01:55:10 +0530

    feat: updated proto-gen script with thrird_party proto gen

--- scripts/proto-gen.sh
+++ scripts/proto-gen.sh
@@ -2,8 +2,16 @@
 
 set -euo pipefail
 
-cd ./proto/ && \
+project_dir=$(pwd)
+
+cd "${project_dir}/third_party/proto/" && \
 buf generate && \
-cd .. && \
+cd ../ && \
+cp -r github.com/sentinel-official/hub/v12/third_party/* ./ && \
+rm -rf github.com/
+
+cd "${project_dir}/proto/" && \
+buf generate && \
+cd ../ && \
 cp -r github.com/sentinel-official/hub/v12/* ./ && \
 rm -rf github.com/
