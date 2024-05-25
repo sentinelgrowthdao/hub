ad96ae2f96d2e25370fafa55d8bd45e68228e75c
Tony Stark <ironman0x7b2@protonmail.com>
2024-05-25 18:37:02 +0530

    feat: add oracle asset keys

---
+++ x/oracle/types/keys.go
@@ -0,0 +1,32 @@
+package types
+
+import (
+	"fmt"
+)
+
+const (
+	ModuleName = "oracle"
+	PortID     = ModuleName
+	Version    = "oracle-1"
+)
+
+var (
+	PortKey = []byte{0x00}
+
+	AssetKeyPrefix           = []byte{0x10}
+	AssetPriceKeyPrefix      = []byte{0x20}
+	DenomForRequestKeyPrefix = []byte{0x30, 0x00}
+)
+
+func AssetKey(denom string) []byte {
+	return append(AssetKeyPrefix, []byte(denom)...)
+}
+
+func AssetPriceKey(denom string) []byte {
+	return append(AssetPriceKeyPrefix, []byte(denom)...)
+}
+
+func DenomForRequestKey(sequence uint64, index int) []byte {
+	s := fmt.Sprintf("%d/%d", sequence, index)
+	return append(DenomForRequestKeyPrefix, []byte(s)...)
+}
