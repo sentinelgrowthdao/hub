4b5a5558e7b8f3b84fd62a7035dd47d1d782a582
Tony Stark <ironman0x7b2@protonmail.com>
2024-05-30 02:00:01 +0530

    feat: added basic required params for oracle module

--- x/oracle/keeper/params.go
+++ x/oracle/keeper/params.go
@@ -1,12 +1,46 @@
 package keeper
 
 import (
+	"time"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/sentinel-official/hub/v12/x/oracle/types"
 )
 
+func (k *Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	var (
+		store = k.Store(ctx)
+		value = k.cdc.MustMarshal(&params)
+	)
+
+	store.Set(types.ParamsKey, value)
+}
+
+func (k *Keeper) GetParams(ctx sdk.Context) (v types.Params) {
+	var (
+		store = k.Store(ctx)
+		value = store.Get(types.ParamsKey)
+	)
+
+	k.cdc.MustUnmarshal(value, &v)
+	return v
+}
+
+func (k *Keeper) GetChannelID(ctx sdk.Context) string {
+	return k.GetParams(ctx).ChannelID
+}
+
 func (k *Keeper) GetPortID(ctx sdk.Context) string {
-	store := k.Store(ctx)
-	return string(store.Get(types.PortIDKey))
+	return k.GetParams(ctx).PortID
+}
+
+func (k *Keeper) GetTimeoutDuration(ctx sdk.Context) time.Duration {
+	return k.GetParams(ctx).TimeoutDuration
+}
+
+func (k *Keeper) GetTimeout(ctx sdk.Context) int64 {
+	return ctx.BlockTime().Add(
+		k.GetTimeoutDuration(ctx),
+	).UnixNano()
 }
