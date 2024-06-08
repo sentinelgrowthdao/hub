63c25ca9f3142be3987a664abfa1e345aa09b70b
Tony Stark <ironman0x7b2@protonmail.com>
2024-06-08 05:45:56 +0530

    chore: moved osmosis proto files to third_party/osmosis folder

--- scripts/proto-gen.sh
+++ scripts/proto-gen.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 project_dir=$(pwd)
 
-cd "${project_dir}/third_party/proto/" && \
+cd "${project_dir}/third_party/osmosis/proto/" && \
 buf generate && \
 cd ../ && \
-cp -r github.com/sentinel-official/hub/v12/third_party/* ./ && \
+cp -r github.com/sentinel-official/hub/v12/third_party/osmosis/* ./ && \
 rm -rf github.com/
 
 cd "${project_dir}/proto/" && \
