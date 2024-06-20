bf51c5625010c0c53156e40270beeb0afd351f0c
Tony Stark <ironman0x7b2@protonmail.com>
2024-06-20 15:45:14 +0530

    feat: add v2 messages in node module

---
+++ x/node/types/v3/msg.go
@@ -0,0 +1,160 @@
+package v3
+
+import (
+	sdkerrors "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	base "github.com/sentinel-official/hub/v12/types"
+	"github.com/sentinel-official/hub/v12/x/node/types"
+)
+
+var (
+	_ sdk.Msg = (*MsgStartLeaseRequest)(nil)
+	_ sdk.Msg = (*MsgRenewLeaseRequest)(nil)
+	_ sdk.Msg = (*MsgEndLeaseRequest)(nil)
+	_ sdk.Msg = (*MsgStartSessionRequest)(nil)
+)
+
+func (m *MsgStartLeaseRequest) ValidateBasic() error {
+	if m.From == "" {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "from cannot be empty")
+	}
+	if _, err := sdk.AccAddressFromBech32(m.From); err != nil {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, err.Error())
+	}
+	if m.NodeAddress == "" {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "node_address cannot be empty")
+	}
+	if _, err := base.NodeAddressFromBech32(m.NodeAddress); err != nil {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, err.Error())
+	}
+	if m.Hours < 0 {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "hours cannot be negative")
+	}
+	if m.Hours == 0 {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "hours cannot be zero")
+	}
+	if m.Denom == "" {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "denom cannot be empty")
+	}
+	if err := sdk.ValidateDenom(m.Denom); err != nil {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, err.Error())
+	}
+
+	return nil
+}
+
+func (m *MsgStartLeaseRequest) GetSigners() []sdk.AccAddress {
+	from, err := sdk.AccAddressFromBech32(m.From)
+	if err != nil {
+		panic(err)
+	}
+
+	return []sdk.AccAddress{from}
+}
+
+func (m *MsgRenewLeaseRequest) ValidateBasic() error {
+	if m.From == "" {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "from cannot be empty")
+	}
+	if _, err := sdk.AccAddressFromBech32(m.From); err != nil {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, err.Error())
+	}
+	if m.ID == 0 {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "id cannot be zero")
+	}
+	if m.Hours < 0 {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "hours cannot be negative")
+	}
+	if m.Hours == 0 {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "hours cannot be zero")
+	}
+	if m.Denom == "" {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "denom cannot be empty")
+	}
+	if err := sdk.ValidateDenom(m.Denom); err != nil {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, err.Error())
+	}
+
+	return nil
+}
+
+func (m *MsgRenewLeaseRequest) GetSigners() []sdk.AccAddress {
+	from, err := sdk.AccAddressFromBech32(m.From)
+	if err != nil {
+		panic(err)
+	}
+
+	return []sdk.AccAddress{from}
+}
+
+func (m *MsgEndLeaseRequest) ValidateBasic() error {
+	if m.From == "" {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "from cannot be empty")
+	}
+	if _, err := sdk.AccAddressFromBech32(m.From); err != nil {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, err.Error())
+	}
+	if m.ID == 0 {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "id cannot be zero")
+	}
+
+	return nil
+}
+
+func (m *MsgEndLeaseRequest) GetSigners() []sdk.AccAddress {
+	from, err := sdk.AccAddressFromBech32(m.From)
+	if err != nil {
+		panic(err)
+	}
+
+	return []sdk.AccAddress{from}
+}
+
+func (m *MsgStartSessionRequest) ValidateBasic() error {
+	if m.From == "" {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "from cannot be empty")
+	}
+	if _, err := sdk.AccAddressFromBech32(m.From); err != nil {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, err.Error())
+	}
+	if m.NodeAddress == "" {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "node_address cannot be empty")
+	}
+	if _, err := base.NodeAddressFromBech32(m.NodeAddress); err != nil {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, err.Error())
+	}
+	if m.Gigabytes == 0 && m.Hours == 0 {
+		return sdkerrors.Wrapf(types.ErrorInvalidMessage, "[gigabytes, hours] cannot be empty")
+	}
+	if m.Gigabytes != 0 && m.Hours != 0 {
+		return sdkerrors.Wrapf(types.ErrorInvalidMessage, "[gigabytes, hours] cannot be non-empty")
+	}
+	if m.Gigabytes != 0 {
+		if m.Gigabytes < 0 {
+			return sdkerrors.Wrap(types.ErrorInvalidMessage, "gigabytes cannot be negative")
+		}
+	}
+	if m.Hours != 0 {
+		if m.Hours < 0 {
+			return sdkerrors.Wrap(types.ErrorInvalidMessage, "hours cannot be negative")
+		}
+	}
+	if m.Denom == "" {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "denom cannot be empty")
+	}
+	if err := sdk.ValidateDenom(m.Denom); err != nil {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, err.Error())
+	}
+
+	return nil
+}
+
+func (m *MsgStartSessionRequest) GetSigners() []sdk.AccAddress {
+	from, err := sdk.AccAddressFromBech32(m.From)
+	if err != nil {
+		panic(err)
+	}
+
+	return []sdk.AccAddress{from}
+}
