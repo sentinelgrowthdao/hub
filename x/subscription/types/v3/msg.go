3c1f020c834c0f24c8af22977237e5aa63c1eb56
Tony Stark <ironman0x7b2@protonmail.com>
2024-06-20 16:00:14 +0530

    feat: add v3 messages in subscription module

---
+++ x/subscription/types/v3/msg.go
@@ -0,0 +1,42 @@
+package v3
+
+import (
+	sdkerrors "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	base "github.com/sentinel-official/hub/v12/types"
+	"github.com/sentinel-official/hub/v12/x/subscription/types"
+)
+
+var (
+	_ sdk.Msg = (*MsgStartSessionRequest)(nil)
+)
+
+func (m *MsgStartSessionRequest) ValidateBasic() error {
+	if m.From == "" {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "from cannot be empty")
+	}
+	if _, err := sdk.AccAddressFromBech32(m.From); err != nil {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, err.Error())
+	}
+	if m.SubscriptionID == 0 {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "subscription_id cannot be zero")
+	}
+	if m.NodeAddress == "" {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, "node_address cannot be empty")
+	}
+	if _, err := base.NodeAddressFromBech32(m.NodeAddress); err != nil {
+		return sdkerrors.Wrap(types.ErrorInvalidMessage, err.Error())
+	}
+
+	return nil
+}
+
+func (m *MsgStartSessionRequest) GetSigners() []sdk.AccAddress {
+	from, err := sdk.AccAddressFromBech32(m.From)
+	if err != nil {
+		panic(err)
+	}
+
+	return []sdk.AccAddress{from}
+}
