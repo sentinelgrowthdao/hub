964585a115c7bf4980eb2ea53b46e009f110c3ba
Tony Stark <ironman0x7b2@protonmail.com>
2024-06-20 21:51:55 +0530

    feat: register v3 services

--- x/subscription/service.go
+++ x/subscription/service.go
@@ -7,8 +7,10 @@ import (
 	"github.com/sentinel-official/hub/v12/x/subscription/keeper"
 	v1keeper "github.com/sentinel-official/hub/v12/x/subscription/keeper/v1"
 	v2keeper "github.com/sentinel-official/hub/v12/x/subscription/keeper/v2"
+	v3keeper "github.com/sentinel-official/hub/v12/x/subscription/keeper/v3"
 	v1types "github.com/sentinel-official/hub/v12/x/subscription/types/v1"
 	v2types "github.com/sentinel-official/hub/v12/x/subscription/types/v2"
+	v3types "github.com/sentinel-official/hub/v12/x/subscription/types/v3"
 )
 
 func RegisterServices(configurator sdkmodule.Configurator, cdc codec.BinaryCodec, k keeper.Keeper) {
@@ -17,4 +19,7 @@ func RegisterServices(configurator sdkmodule.Configurator, cdc codec.BinaryCodec
 
 	v2types.RegisterMsgServiceServer(configurator.MsgServer(), v2keeper.NewMsgServiceServer(k))
 	v2types.RegisterQueryServiceServer(configurator.QueryServer(), v2keeper.NewQueryServiceServer(cdc, k))
+
+	v3types.RegisterMsgServiceServer(configurator.MsgServer(), v3keeper.NewMsgServiceServer(k))
+	v3types.RegisterQueryServiceServer(configurator.QueryServer(), v3keeper.NewQueryServiceServer(cdc, k))
 }
