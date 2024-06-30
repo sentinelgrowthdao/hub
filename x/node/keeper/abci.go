fe13841ac8a738810967a78eb5de351f594214ef
Tony Stark <ironman0x7b2@protonmail.com>
2024-06-30 19:40:21 +0530

    feat: add lease related handlers in abci begin block

--- x/node/keeper/abci.go
+++ x/node/keeper/abci.go
@@ -3,106 +3,74 @@ package keeper
 import (
 	"time"
 
-	abcitypes "github.com/cometbft/cometbft/abci/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	v1base "github.com/sentinel-official/hub/v12/types/v1"
+	baseutils "github.com/sentinel-official/hub/v12/utils"
 	"github.com/sentinel-official/hub/v12/x/node/types/v2"
+	"github.com/sentinel-official/hub/v12/x/node/types/v3"
 )
 
-func (k *Keeper) EndBlock(ctx sdk.Context) []abcitypes.ValidatorUpdate {
+func (k *Keeper) handlePriceUpdates(ctx sdk.Context) {
 	var (
-		maxGigabytePricesModified = k.IsMaxGigabytePricesModified(ctx)
 		minGigabytePricesModified = k.IsMinGigabytePricesModified(ctx)
-		maxHourlyPricesModified   = k.IsMaxHourlyPricesModified(ctx)
 		minHourlyPricesModified   = k.IsMinHourlyPricesModified(ctx)
 	)
 
-	if maxGigabytePricesModified || minGigabytePricesModified || maxHourlyPricesModified || minHourlyPricesModified {
-		maxGigabytePrices := sdk.NewCoins()
-		if maxGigabytePricesModified {
-			maxGigabytePrices = k.MaxGigabytePrices(ctx)
-		}
-
-		minGigabytePrices := sdk.NewCoins()
-		if minGigabytePricesModified {
-			minGigabytePrices = k.MinGigabytePrices(ctx)
-		}
-
-		maxHourlyPrices := sdk.NewCoins()
-		if maxHourlyPricesModified {
-			maxHourlyPrices = k.MaxHourlyPrices(ctx)
-		}
-
-		minHourlyPrices := sdk.NewCoins()
-		if minHourlyPricesModified {
-			minHourlyPrices = k.MinHourlyPrices(ctx)
-		}
-
-		k.IterateNodes(ctx, func(_ int, item v2.Node) bool {
-			k.Logger(ctx).Info("Updating prices for node", "address", item.Address)
-
-			if maxGigabytePricesModified {
-				for _, coin := range maxGigabytePrices {
-					amount := item.GigabytePrices.AmountOf(coin.Denom)
-					if amount.GT(coin.Amount) {
-						item.GigabytePrices = item.GigabytePrices.Sub(
-							sdk.NewCoin(coin.Denom, amount),
-						).Add(coin)
-					}
-				}
-			}
-
-			if minGigabytePricesModified {
-				for _, coin := range minGigabytePrices {
-					amount := item.GigabytePrices.AmountOf(coin.Denom)
-					if amount.LT(coin.Amount) {
-						item.GigabytePrices = item.GigabytePrices.Sub(
-							sdk.NewCoin(coin.Denom, amount),
-						).Add(coin)
-					}
-				}
-			}
-
-			if maxHourlyPricesModified {
-				for _, coin := range maxHourlyPrices {
-					amount := item.HourlyPrices.AmountOf(coin.Denom)
-					if amount.GT(coin.Amount) {
-						item.HourlyPrices = item.HourlyPrices.Sub(
-							sdk.NewCoin(coin.Denom, amount),
-						).Add(coin)
-					}
-				}
-			}
-
-			if minHourlyPricesModified {
-				for _, coin := range minHourlyPrices {
-					amount := item.HourlyPrices.AmountOf(coin.Denom)
-					if amount.LT(coin.Amount) {
-						item.HourlyPrices = item.HourlyPrices.Sub(
-							sdk.NewCoin(coin.Denom, amount),
-						).Add(coin)
-					}
-				}
-			}
-
-			k.SetNode(ctx, item)
-			ctx.EventManager().EmitTypedEvent(
-				&v2.EventUpdateDetails{
-					Address:        item.Address,
-					GigabytePrices: item.GigabytePrices.String(),
-					HourlyPrices:   item.HourlyPrices.String(),
-				},
-			)
-
-			return false
-		})
+	if !minGigabytePricesModified && !minHourlyPricesModified {
+		return
 	}
 
-	k.IterateNodesForInactiveAt(ctx, ctx.BlockTime(), func(_ int, item v2.Node) bool {
-		k.Logger(ctx).Info("Found an inactive node", "address", item.Address)
+	minGigabytePrices := sdk.NewCoins()
+	if minGigabytePricesModified {
+		minGigabytePrices = k.MinGigabytePrices(ctx)
+	}
 
+	minHourlyPrices := sdk.NewCoins()
+	if minHourlyPricesModified {
+		minHourlyPrices = k.MinHourlyPrices(ctx)
+	}
+
+	k.IterateNodes(ctx, func(_ int, item v2.Node) bool {
+		if minGigabytePricesModified {
+			for _, coin := range minGigabytePrices {
+				amount := item.GigabytePrices.AmountOf(coin.Denom)
+				if amount.LT(coin.Amount) {
+					item.GigabytePrices = item.GigabytePrices.Sub(
+						sdk.NewCoin(coin.Denom, amount),
+					).Add(coin)
+				}
+			}
+		}
+
+		if minHourlyPricesModified {
+			for _, coin := range minHourlyPrices {
+				amount := item.HourlyPrices.AmountOf(coin.Denom)
+				if amount.LT(coin.Amount) {
+					item.HourlyPrices = item.HourlyPrices.Sub(
+						sdk.NewCoin(coin.Denom, amount),
+					).Add(coin)
+				}
+			}
+		}
+
+		k.SetNode(ctx, item)
+		ctx.EventManager().EmitTypedEvent(
+			&v2.EventUpdateDetails{
+				Address:        item.Address,
+				GigabytePrices: item.GigabytePrices.String(),
+				HourlyPrices:   item.HourlyPrices.String(),
+			},
+		)
+
+		return false
+	})
+}
+
+func (k *Keeper) handleInactiveNodes(ctx sdk.Context) {
+	k.IterateNodesForInactiveAt(ctx, ctx.BlockTime(), func(_ int, item v2.Node) bool {
 		nodeAddr := item.GetAddress()
+
 		k.DeleteActiveNode(ctx, nodeAddr)
 		k.DeleteNodeForInactiveAt(ctx, item.InactiveAt, nodeAddr)
 
@@ -120,6 +88,93 @@ func (k *Keeper) EndBlock(ctx sdk.Context) []abcitypes.ValidatorUpdate {
 
 		return false
 	})
+}
 
-	return nil
+func (k *Keeper) handleInactiveLeases(ctx sdk.Context) {
+	k.IterateLeasesForInactiveAt(ctx, ctx.BlockTime(), func(_ int, item v3.Lease) bool {
+		k.DeleteLeaseForInactiveAt(ctx, item.InactiveAt, item.ID)
+
+		if err := k.LeaseInactivePreHook(ctx, item.ID); err != nil {
+			panic(err)
+		}
+
+		var (
+			nodeAddr = item.GetNodeAddress()
+			provAddr = item.GetProvAddress()
+		)
+
+		k.DeleteLease(ctx, item.ID)
+		k.DeleteLeaseForNode(ctx, nodeAddr, item.ID)
+		k.DeleteLeaseForPayoutAt(ctx, item.PayoutAt, item.ID)
+		k.DeleteLeaseForProvider(ctx, provAddr, item.ID)
+		k.DeleteLeaseForProviderByNode(ctx, provAddr, nodeAddr, item.ID)
+		k.DeleteLeaseForRenewAt(ctx, item.RenewAt, item.ID)
+
+		return false
+	})
+}
+
+func (k *Keeper) handleLeasePayouts(ctx sdk.Context) {
+	k.IterateLeasesForPayoutAt(ctx, ctx.BlockTime(), func(_ int, item v3.Lease) (stop bool) {
+		k.DeleteLeaseForPayoutAt(ctx, item.PayoutAt, item.ID)
+
+		var (
+			nodeAddr     = item.GetNodeAddress()
+			provAddr     = item.GetProvAddress()
+			stakingShare = k.StakingShare(ctx)
+		)
+
+		stakingReward := baseutils.GetProportionOfCoin(item.Price, stakingShare)
+		if err := k.SendCoinFromDepositToModule(ctx, provAddr.Bytes(), k.feeCollectorName, stakingReward); err != nil {
+			panic(err)
+		}
+
+		payment := item.Price.Sub(stakingReward)
+		if err := k.SendCoinFromDepositToAccount(ctx, provAddr.Bytes(), nodeAddr.Bytes(), payment); err != nil {
+			panic(err)
+		}
+
+		item.Hours = item.Hours + 1
+		if item.Hours < item.MaxHours {
+			item.PayoutAt = item.PayoutAt.Add(time.Hour)
+		} else {
+			item.PayoutAt = time.Time{}
+		}
+
+		k.SetLease(ctx, item)
+
+		if item.Hours < item.MaxHours {
+			k.SetLeaseForPayoutAt(ctx, item.PayoutAt, item.ID)
+		}
+
+		return false
+	})
+}
+
+func (k *Keeper) handleLeaseRenewals(ctx sdk.Context) {
+	k.IterateLeasesForRenewAt(ctx, ctx.BlockTime(), func(_ int, item v3.Lease) bool {
+		k.DeleteLeaseForRenewAt(ctx, item.RenewAt, item.ID)
+
+		msg := &v3.MsgRenewLeaseRequest{
+			From:      "",
+			ID:        item.ID,
+			Hours:     item.MaxHours,
+			Denom:     item.Price.Denom,
+			Renewable: true,
+		}
+
+		if _, err := k.RenewLease(ctx, msg); err != nil {
+			panic(err)
+		}
+
+		return false
+	})
+}
+
+func (k *Keeper) BeginBlock(ctx sdk.Context) {
+	k.handleInactiveLeases(ctx)
+	k.handleInactiveNodes(ctx)
+	k.handleLeasePayouts(ctx)
+	k.handleLeaseRenewals(ctx)
+	k.handlePriceUpdates(ctx)
 }
