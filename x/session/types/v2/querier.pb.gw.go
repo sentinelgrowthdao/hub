237545659387c740c4ec7d975d4f55558ddb102a
Tony Stark <ironman0x7b2@protonmail.com>
2024-07-07 19:01:17 +0530

    chore: added version in the v2 REST API endpoints of session module

--- x/session/types/v2/querier.pb.gw.go
+++ x/session/types/v2/querier.pb.gw.go
@@ -803,19 +803,19 @@ func RegisterQueryServiceHandlerClient(ctx context.Context, mux *runtime.ServeMu
 }
 
 var (
-	pattern_QueryService_QuerySessions_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"sentinel", "sessions"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_QueryService_QuerySessions_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"sentinel", "session", "v2", "sessions"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_QueryService_QuerySessionsForAccount_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"sentinel", "accounts", "address", "sessions"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_QueryService_QuerySessionsForAccount_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5}, []string{"sentinel", "session", "v2", "accounts", "address", "sessions"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_QueryService_QuerySessionsForNode_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"sentinel", "nodes", "address", "sessions"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_QueryService_QuerySessionsForNode_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5}, []string{"sentinel", "session", "v2", "nodes", "address", "sessions"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_QueryService_QuerySessionsForSubscription_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"sentinel", "subscriptions", "id", "sessions"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_QueryService_QuerySessionsForSubscription_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5}, []string{"sentinel", "session", "v2", "subscriptions", "id", "sessions"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_QueryService_QuerySessionsForAllocation_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5}, []string{"sentinel", "subscriptions", "id", "allocations", "address", "sessions"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_QueryService_QuerySessionsForAllocation_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5, 1, 0, 4, 1, 5, 6, 2, 7}, []string{"sentinel", "session", "v2", "subscriptions", "id", "allocations", "address", "sessions"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_QueryService_QuerySession_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"sentinel", "sessions", "id"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_QueryService_QuerySession_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"sentinel", "session", "v2", "sessions", "id"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_QueryService_QueryParams_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"sentinel", "modules", "session", "params"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_QueryService_QueryParams_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"sentinel", "session", "v2", "params"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
